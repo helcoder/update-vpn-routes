1c2340d2a820cb5a1942bba20ba93a740ea1bd39
helcoder <helcoder@gmail.com>
2023-08-31 16:02:45 +0400

    allow to configure routes per file

--- UpdateOpenVPNRoutes.ps1
+++ UpdateOpenVPNRoutes.ps1
@@ -1,27 +1,63 @@
 $settings = Get-Content "$PSScriptRoot\settings.json" | ConvertFrom-Json
-$OVPNFile = $settings.OVPNFile
-[string[]]$hosts = $settings.hosts
-$newRoutes = ""
+$items = $settings.items;
+$hash = @{}
+$fileHash = @{}
 
 Write-Host "Resolving hosts..."
 
-foreach ($hostsItem in $hosts) {
-	$dns = Resolve-DnsName $hostsItem -Type A
+foreach ($item in $items) {
+	$itemHash = @{}
 
-	foreach ($dnsItem in $dns) {
-		if ($dnsItem.Type -eq "A") {
-			$item = "`n#$hostsItem`nroute $($dnsItem.IPAddress)"
-			$newRoutes += $item
-			Write-Host $item -NoNewline
+	foreach ($hostsItem in $item.hosts) {
+		$dns = Resolve-DnsName $hostsItem -Type A
+
+		foreach ($dnsItem in $dns) {
+			if ($dnsItem.Type -eq "A") {
+				if ($hash.Keys -notcontains $dnsItem.IPAddress) {
+					$hash[$dnsItem.IPAddress] = @()
+				}
+
+				$hash[$dnsItem.IPAddress] += $hostsItem
+
+				$itemHash[$dnsItem.IPAddress] = $hash[$dnsItem.IPAddress]
+
+				$text = "`n#$hostsItem`nroute $($dnsItem.IPAddress)"
+				Write-Host $text -NoNewline
+			}
+		}
+	}
+
+	foreach ($file in $item.files) {
+		if ($fileHash.Keys -notcontains $file) {
+			$fileHash[$file] = @{}
+		}
+
+		foreach ($ip in $itemHash.Keys) {
+			$fileHash[$file][$ip] = $itemHash[$ip]
 		}
 	}
 }
 
-if (Test-Path $OVPNFile) {
-	(Get-Content $OVPNFile -Raw) -replace '(?s)(#BEGINAUTOROUTES).*?(#ENDAUTOROUTES)', "`$1`n$newRoutes`n`n`$2" | Out-File $OVPNFile
-	Write-Host "`n`n$OVPNFile has been updated"
-} else {
-	Write-Host "`n`nUnable to update $OVPNFile because it doesn't exist"
+foreach ($file in $fileHash.Keys) {
+	$newRoutes = ""
+
+	foreach ($ip in $fileHash[$file].Keys) {
+		$item = ""
+
+		foreach ($h in $fileHash[$file][$ip]) {
+			$item += "`n#$h"
+		}
+
+		$item += "`nroute $ip"
+		$newRoutes += $item
+	}
+
+	if (Test-Path $file) {
+		(Get-Content $file -Raw) -replace '(?s)(#BEGINAUTOROUTES).*?(#ENDAUTOROUTES)', "`$1`n$newRoutes`n`n`$2" | Out-File $file -encoding utf8
+		Write-Host "`n`n$file has been updated"
+	} else {
+		Write-Host "`n`nUnable to update $file because it doesn't exist"
+	}
 }
 
 Write-Host "`nPress any key to exit..."
